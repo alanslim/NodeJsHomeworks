0b4874e01a4beea4a1dae3ef6a282d7a80bbd203
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-06-20 16:50:43 +0600

    Проверено дз от 12.06
    Комментарии к коду

--- node filewriter hw/dz2/main.js
+++ node filewriter hw/dz2/main.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 const log = console.log;
 
 fs.readFile('file.json', 'utf-8', (err, data) => {
-    if (err) throw err;
+    if (err) throw err;  // VN: бросать исключение там, где можно просто вывести сообщение об ошибке, нехорошо
     try {
         const json = JSON.parse(data);
 
@@ -12,7 +12,7 @@ fs.readFile('file.json', 'utf-8', (err, data) => {
 
 
         fs.writeFile('file.json', JSON.stringify(json), 'utf-8', (err) => {
-            if (err) throw err;
+            if (err) throw err;  // VN: то же самое
         });
 
         log('Файл успешно обновлен');
